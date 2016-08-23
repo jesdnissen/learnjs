510e773787804c2fc2b1145d1408a24a4e7e8b38
jesdnissen <jesdnissen@gmail.com>
2016-08-23 13:18:28 +0200

    page 39

--- public/app.js
+++ public/app.js
@@ -1,11 +1,19 @@
 'use strict';
+
 var learnjs = {};
-learnjs.problemView = function() {
-	 return $('<div class="problem-view">').text('Kommer snart');
+
+learnjs.problemView = function(problemNumber) {
+	var title = 'Problem #' + problemNumber + ' Kommer snart!';
+	 return $('<div class="problem-view">').text(title);
 }
 learnjs.showView = function(hash) {
 	var routes = {
-		'#problem-1').empty().append(viewFn());
+		'#problem': learnjs.problemView
+	};
+	var hashParts = hash.split('-');
+	var viewFn = routes[hashParts[0]];
+	if (viewFn) {
+		$('.view-container').empty().append(viewFn(hashParts[1]));
 	}
 }
 
--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -4,8 +4,19 @@ describe('LearnJS', function() {
 		expect($('.view-container .problem-view').length).toEqual(1);
 	});
 	it('Shows the landing page view when there is no hash', function() {
-		learnjs.showview('');
-		expect($('.view-container .landingview').length).toEqual(1);
+		learnjs.showView('');
+		expect($('.view-container .landing-view').length).toEqual(1);
+	});
+	it('passes the hash view parameter to the view function', function() {
+		spyOn(learnjs, 'problemView');
+		learnjs.showView('#problem-42');
+		expect(learnjs.problemView).toHaveBeenCalledWith('42');
+	});
+	describe('problem view', function() {
+		it('has a title that includes the problem number', function() {
+			var view = learnjs.problemView('1');
+			expect(view.text()).toEqual('Problem #1 Kommer snart!');
+		});
 	});
 });
 
